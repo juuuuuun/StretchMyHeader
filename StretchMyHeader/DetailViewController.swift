3b4dda4023c38ad31a4ba03bdf089eb0ae214796
Jun Oh <kyungjunjoh@gmail.com>
2019-02-19 11:05:22 -0800

    Initial Commit

---
+++ StretchMyHeader/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  StretchMyHeader
+//
+//  Created by Jun Oh on 2019-02-19.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = detailItem {
+        if let label = detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    configureView()
+  }
+
+  var detailItem: NSDate? {
+    didSet {
+        // Update the view.
+        configureView()
+    }
+  }
+
+
+}
+
